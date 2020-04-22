3e0ff45ab1f46174694b9e96925b59a1b38a2558
lihaoyu <505571317@qq.com>
2020-04-22 01:20:44 -0500

    add sha_256 and sha_1

--- hash.h
+++ hash.h
@@ -2,6 +2,7 @@
 #define HASH_H
 
 #include <string>
+#include <openssl/sha.h>
 
 typedef void (*hash_func)(char*, int, char*);
 
@@ -15,12 +16,17 @@ inline void identity_mapping(char* orig, int orig_len, char* hashed) {
     hashed = orig;
 }
 
-inline void sha_0(char* orig, int orig_len, char* hashed) {
+inline void sha_256(char* orig, int orig_len, char* hashed) {
     // @TODO
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, orig, orig_len);
+    SHA256_Final(hashed, &sha256);
 }
 
 inline void sha_1(char* orig, int orig_len, char* hashed) {
     // @TODO
+    SHA1(orig, orig_len, hashed);
 }
 
 #endif
